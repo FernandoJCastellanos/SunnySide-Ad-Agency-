bf341c90b9cbdd1146f623be97a454f70d11fa40
FernandoJC <fernandojcastellanos@gmail.com>
2023-01-02 15:13:52 -0400

    initial commit

---
+++ java/indexjava.js
@@ -0,0 +1,11 @@
+
+const navActive = () => {
+    const burger = document.querySelector(".burger");
+    const navList = document.querySelector(".nav-list");
+
+    burger.addEventListener("click", () => {
+        navList.classList.toggle("nav-active");
+    });
+};
+
+navActive();
